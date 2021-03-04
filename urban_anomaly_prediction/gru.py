ac054ed9c888c44049429830236c9fb112a75131
mzj <zijiamm@163.com>
2021-03-04 23:01:25 +0800

    update

---
+++ urban_anomaly_prediction/gru.py
@@ -0,0 +1,167 @@
+import os
+import torch
+from torch import nn
+from torch.autograd import Variable
+
+class GRUCell(nn.Module):
+    def __init__(self, input_size, input_dim, hidden_dim, kernel_size, bias, dtype):
+
+        super(GRUCell, self).__init__()
+        self.height, self.width = input_size
+        self.padding = kernel_size[0] // 2, kernel_size[1] // 2
+        self.hidden_dim = hidden_dim
+        self.bias = bias
+        self.dtype = dtype
+
+        self.conv_gates = nn.Conv2d(in_channels=input_dim + hidden_dim,
+                                    out_channels=2*self.hidden_dim,  # for update_gate,reset_gate respectively
+                                    kernel_size=kernel_size,
+                                    padding=self.padding,
+                                    bias=self.bias)
+
+        self.conv_can = nn.Conv2d(in_channels=input_dim+hidden_dim,
+                              out_channels=self.hidden_dim, # for candidate neural memory
+                              kernel_size=kernel_size,
+                              padding=self.padding,
+                              bias=self.bias)
+
+    def init_hidden(self, batch_size):
+        return (Variable(torch.zeros(batch_size, self.hidden_dim, self.height, self.width)).type(self.dtype))
+
+    def forward(self, input_tensor, h_cur):
+
+        combined = torch.cat([input_tensor, h_cur], dim=1)
+        combined_conv = self.conv_gates(combined)
+
+        gamma, beta = torch.split(combined_conv, self.hidden_dim, dim=1)
+        reset_gate = torch.sigmoid(gamma)
+        update_gate = torch.sigmoid(beta)
+
+        combined = torch.cat([input_tensor, reset_gate*h_cur], dim=1)
+        cc_cnm = self.conv_can(combined)
+        cnm = torch.tanh(cc_cnm)
+
+        h_next = (1 - update_gate) * h_cur + update_gate * cnm
+        return h_next
+
+
+class GRU(nn.Module):
+    def __init__(self, input_size, input_dim, hidden_dim, kernel_size, num_layers,
+                 dtype, batch_first=False, bias=True, return_all_layers=False):
+
+        super(GRU, self).__init__()
+
+
+        kernel_size = self._extend_for_multilayer(kernel_size, num_layers)
+        hidden_dim  = self._extend_for_multilayer(hidden_dim, num_layers)
+        if not len(kernel_size) == len(hidden_dim) == num_layers:
+            raise ValueError('Inconsistent list length.')
+
+        self.height, self.width = input_size
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.kernel_size = kernel_size
+        self.dtype = dtype
+        self.num_layers = num_layers
+        self.batch_first = batch_first
+        self.bias = bias
+        self.return_all_layers = return_all_layers
+
+        cell_list = []
+        for i in range(0, self.num_layers):
+            cur_input_dim = input_dim if i == 0 else hidden_dim[i - 1]
+            cell_list.append(GRUCell(input_size=(self.height, self.width),
+                                         input_dim=cur_input_dim,
+                                         hidden_dim=self.hidden_dim[i],
+                                         kernel_size=self.kernel_size[i],
+                                         bias=self.bias,
+                                         dtype=self.dtype))
+
+        self.cell_list = nn.ModuleList(cell_list)
+
+    def forward(self, input_tensor, hidden_state=None):
+
+        if not self.batch_first:
+
+            input_tensor = input_tensor.permute(1, 0, 2, 3, 4)
+
+        if hidden_state is not None:
+            raise NotImplementedError()
+        else:
+            hidden_state = self._init_hidden(batch_size=input_tensor.size(0))
+
+        layer_output_list = []
+        last_state_list   = []
+
+        seq_len = input_tensor.size(1)
+        cur_layer_input = input_tensor
+
+        for layer_idx in range(self.num_layers):
+            h = hidden_state[layer_idx]
+            output_inner = []
+            for t in range(seq_len):
+                h = self.cell_list[layer_idx](input_tensor=cur_layer_input[:, t, :, :, :], # (b,t,c,h,w)
+                                              h_cur=h)
+                output_inner.append(h)
+
+            layer_output = torch.stack(output_inner, dim=1)
+            cur_layer_input = layer_output
+
+            layer_output_list.append(layer_output)
+            last_state_list.append([h])
+
+        if not self.return_all_layers:
+            layer_output_list = layer_output_list[-1:]
+            last_state_list   = last_state_list[-1:]
+
+        return layer_output_list, last_state_list
+
+    def _init_hidden(self, batch_size):
+        init_states = []
+        for i in range(self.num_layers):
+            init_states.append(self.cell_list[i].init_hidden(batch_size))
+        return init_states
+
+    @staticmethod
+    def _check_kernel_size_consistency(kernel_size):
+        if not (isinstance(kernel_size, tuple) or
+                    (isinstance(kernel_size, list) and all([isinstance(elem, tuple) for elem in kernel_size]))):
+            raise ValueError('`kernel_size` must be tuple or list of tuples')
+
+    @staticmethod
+    def _extend_for_multilayer(param, num_layers):
+        if not isinstance(param, list):
+            param = [param] * num_layers
+        return param
+
+
+if __name__ == '__main__':
+    # set CUDA device
+    os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+
+    # detect if CUDA is available or not
+    use_gpu = torch.cuda.is_available()
+    if use_gpu:
+        dtype = torch.cuda.FloatTensor # computation in GPU
+    else:
+        dtype = torch.FloatTensor
+
+    height = width = 6
+    channels = 256
+    hidden_dim = [32, 64]
+    kernel_size = (3,3) # kernel size for two stacked hidden layer
+    num_layers = 2 # number of stacked hidden layer
+    model = GRU(input_size=(height, width),
+                    input_dim=channels,
+                    hidden_dim=hidden_dim,
+                    kernel_size=kernel_size,
+                    num_layers=num_layers,
+                    dtype=dtype,
+                    batch_first=True,
+                    bias = True,
+                    return_all_layers = False)
+
+    batch_size = 1
+    time_steps = 1
+    input_tensor = torch.rand(batch_size, time_steps, channels, height, width)  # (b,t,c,h,w)
+    layer_output_list, last_state_list = model(input_tensor)
